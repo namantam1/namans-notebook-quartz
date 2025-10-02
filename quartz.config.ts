af31759e186e53afe6392bcc7d16d69360db926c
Naman Tamrakar <namantam1@gmail.com>
2025-10-03 03:55:35 +0530

    Update page title and base URL in Quartz configuration

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Naman's Garden",
+    pageTitleSuffix: " | Naman's Notebook",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "namantam1.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
